8a38bec26076428d362be2077b96ce903aea5a82
Michal Borovka <michalborovka@Michals-MacBook-Air.local>
2024-06-18 08:38:57 +0200

    Updatte from Mac 18.6

--- M_1/Print_f/ft_printf.c
+++ M_1/Print_f/ft_printf.c
@@ -11,199 +11,56 @@
 /* ************************************************************************** */
 
 #include <stdarg.h>
-/*#include "libft.h"*/
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-
-
-/*
-Here starts ft_strlen
-*/
-int	ft_strlen(const char *s)
+static int ft_format(va_list args, const char format)
 {
-	int	i;
+	void *p;
 
-	i = 0;
-	while (s[i])
-		i++;
-	return (i);
-}
-/*
-Here ends ft_strlen
-*/
-
-/*
-Here starts ft_memcpy
-*/
-
-void	*ft_memcpy(void *dest, const void *src, size_t n)
-{
-	size_t			i;
-	unsigned char	*s;
-	unsigned char	*d;
-
-	i = 0;
-	s = (unsigned char *) src;
-	d = (unsigned char *) dest;
-	if (!dest && !src)
-		return (0);
-	while (n > i)
-	{
-		d[i] = s[i];
-		i++;
-	}
-	return (d);
-}
-/*
-Here ends ft_memcpy
-*/
-
-/*
-Here starts ft_substr
-*/
-char	*ft_substr(char const *s, unsigned int start, size_t len)
-{
-	char			*ptr;
-	unsigned int	dest;
-
-	dest = ft_strlen(s);
-	if (!s)
-		return (NULL);
-	if (dest < start)
-		len = 0;
-	else if (len > (dest - start))
-		len = dest - start;
-	ptr = (char *) malloc (sizeof(char) * (len + 1));
-	if (!ptr)
-		return (NULL);
-	ft_memcpy (ptr, s + start, len);
-	ptr[len] = '\0';
-	return (ptr);
-}
-/*
-Here ends ft_substr
-*/
-
-
-/*
-Here starts ft_strchr
-*/
-char	*ft_strchr(const char *s, int c)
-{
-	char	*n;
-
-	n = (char *)s;
-	while (*n != '\0' && *n != (char)c)
-		n++;
-	if (*n == (char)c)
-		return (n);
-	return (NULL);
-}
-/*
-Here ends ft_strchr
-*/
-
-/*
-Here starts ft_split
-*/
-int	ft_wordcount(char const *s, char c)
-{
-	size_t	count;
-
-	if (!*s)
-		return (0);
-	count = 0;
-	while (*s)
-	{
-		while (*s == c)
-			s++;
-		if (*s)
-			count++;
-		while (*s != c && *s)
-			s++;
-	}
-	return (count);
-}
-
-char	**ft_split(char const *s, char c)
-{
-	char	**thewhole;
-	size_t	n;
-	size_t	i;
-
-	i = 0;
-	n = 0;
-	thewhole = (char **)malloc(sizeof(char *) * (ft_wordcount(s, c) + 1));
-	if (!thewhole)
-		return (0);
-	while (*s)
-	{
-		while (*s == c && *s)
-			s++;
-		if (*s)
-		{
-			if (!ft_strchr(s, c))
-				n = ft_strlen(s);
-			else
-				n = ft_strchr(s, c) - s;
-			thewhole[i++] = ft_substr(s, 0, n);
-			s += n;
-		}
-	}
-	thewhole[i] = NULL;
-	return (thewhole);
-}
-
-/*
-Here ends ft_split
-*/
-
-/*
-Here starts the task the FT_PRINT
-*/
-int ft_format(va_list args, const char format)
-{
 	if (format == 'c')
-		return (ft_putchar((va_args(args, int))));
+		return (ft_putchar((va_arg(args, int))));
 	else if (format == 's')
 		return (ft_putstr((va_arg(args, char *))));
 	else if (format == 'p')
+	{
 		p = va_arg(args, void *);
 		if (p)
 			return (ft_hexaddress(p));
 		return (ft_putstr("0x0"));
+	}
 	else if (format == 'd' || format == 'i')
-		return (ft_nbr((va_args(args, int))));
+		return (ft_putnbr((va_arg(args, int))));
 	else if (format == 'u')
-		return (ft_u_putnbr((va_args(args, unsigned int))));
-	else if (format == 'x' || format == 'X');
-		ft_puthex (format);
-		data_type = va_arg(args, unsigned int);
+		return (ft_u_putnbr((va_arg(args, unsigned int))));
+	else if (format == 'x' || format == 'X')
+		return (ft_puthex (format));
 	else if (format == '%')
-		data_type = va_arg(args, unsigned int);
+		return (ft_putchar('%'));
+	else
+		return (-1);
 }
 
 int	ft_printf(const char *format, ...)
 {
 	int	i;
-	va_list args;
+	va_list arg;
 	int len_str;
 
-	va_start(args, format);
+	va_start(arg, format);
 	i = 0;
 	while (format[i])
 	{
 		if (format[i] == '%')
 		{
 			i++;
-			len_str = len_str + ft_format(args, format[i]);
+			len_str = len_str + ft_format(arg, format[i]);
 		}
 		else
 			len_str = len_str + ft_putchar(format[i]);
 		i++;
 	}
-	va_end (args);
+	va_end (arg);
 	return (len_str);
 }
